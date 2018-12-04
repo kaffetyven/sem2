062d0971e76f6db1167ba978c7a6b2f19e089740
kaffetyven <morten_wa@hotmail.com>
2018-12-04 10:36:29 +0100

    set up dice rolling with animations in modal in game window

--- game/scripts/game.js
+++ game/scripts/game.js
@@ -1,7 +1,17 @@
 let playerIndex = "";
 let start = document.getElementById('start');
-let playerOne = ""
-let playerTwo = ""
+let playerOne = "";
+let playerTwo = "";
+let counter = "";
+var diceImg = "";
+var mobileButton = document.getElementById('mobile__button');
+var desktopButton = document.getElementById('desktop__button');
+var mobileButtonText = document.querySelectorAll(".button")[1];
+var desktopButtonText = document.querySelectorAll(".button")[0];
+var turn = "";
+var player= "";
+var mediaTest = window.matchMedia("(max-width: 1023px)");
+var statusText = document.querySelectorAll(".status__text")[0];
 window.addEventListener("load", loadTokens);
 function loadTokens(){
 	if (sessionStorage.getItem("players") == null){
@@ -30,20 +40,115 @@ function loadTokens(){
 		}
 		playerTwoToken.className = "player"+" player2";
 		start.appendChild(playerTwoToken);
-	}
 
+	}
+	setPlayer();
 }
 
+function setPlayer(){
+	if (playerIndex == 1){
+		console.log("its 1 player");
+		player = document.querySelectorAll(".player1")[0];
+		statusText.innerHTML = "Its your turn";
+	}else{
+		if (turn % 2 == 1) {
+			player = document.querySelectorAll(".player2")[0];
+			statusText.innerHTML = playerTwo.name + "s turn"
+		}
+		else {
+			player = document.querySelectorAll(".player1")[0];
+			statusText.innerHTML= playerOne.name +"s turn";
 
-var turn = 0;
+		}
+	}
+}
 function Roll(){	
 	var dice = Math.floor((Math.random() * 6) + 1);	
-	var counter = dice;
-	console.log(counter);
-	if (turn % 2 == 1) {
-		var player = document.querySelectorAll(".player1")[0];
-	} else {var player = document.querySelectorAll(".player2")[0];}
+	counter = dice;	
+	mobileButton.removeAttribute("onclick");
+	desktopButton.removeAttribute("onclick");
+	mobileButton.style.display= "none";
+	desktopButton.style.display= "none";
+	Dice();
+}
+var diceIteration = 1;
+var diceInterval = "";
 
+function Dice(){
+	statusText.innerHTML = "Rolling..."
+	diceInterval = setInterval(function(){		
+		diceImg = document.querySelectorAll(".dice")[0];
+		diceImg.src = "dice/dice"+diceIteration+".png";
+		if (diceIteration == 6) {
+			diceIteration = 1;
+		}else{
+			diceIteration += 1;
+		}
+	}, 100);
+	console.log(counter);
+	var modal = document.querySelectorAll(".modal")[0];
+	var progress = document.querySelectorAll(".progress")[0];
+	modal.style.display="flex";	
+	progress.addEventListener("animationend", Continue);
+}
+function Continue(){
+	console.log("finished");
+	statusText.innerHTML = "You have rolled: "+counter;
+	clearInterval(diceInterval);
+	diceImg.src = "dice/dice"+ counter + ".png";
+	mobileButton.setAttribute("onclick", "ModalAway()");
+	desktopButton.setAttribute("onclick", "ModalAway()");
+	mobileButtonText.innerHTML = "Continue";
+	desktopButtonText.innerHTML = "Continue";
+	mobileButton.style.display = "flex";
+	desktopButton.style.display = "flex";
+	Mediacheck(mediaTest);
+}
+function Mediacheck(x){
+	if (x.matches){
+		desktopButton.style.display = "none";
+		console.log("match");
+	}
+	else{
+		mobileButton.style.display = "none";
+		console.log("does not match");
+	}
+}
+function ModalAway(){
+	mobileButton.style.display= "none";
+	desktopButton.style.display= "none";
+	var modal = document.querySelectorAll(".modal")[0];
+	modal.style.display="none";
+	Footsteps();
+}
+var steps = 0;
+var stepImg = document.querySelectorAll('.footstep')[0];
+
+function Footsteps(){
+	statusText.innerHTML = "Moving...";
+	var stepper = setInterval(function(){
+		if (steps==0) {
+			var currentTile = player.parentNode;
+		}else{
+			var currentTile = stepImg.parentNode;
+		}		
+		var newStepImg = document.createElement('img');
+		newStepImg.src = "asset/stepright.png"
+		newStepImg.className= "footstep";
+		var currentRow = currentTile.parentNode;
+		var nextTile = currentTile.nextSibling.nextSibling;
+		if (nextTile == null) {
+			var nextRow = currentRow.previousSibling.previousSibling;
+			nextRow.firstChild.nextSibling.appendChild(newStepImg);
+		}
+		else{
+		nextTile.appendChild(newStepImg);
+		}
+		steps += 1;
+	}, 1000);
+	
+}
+function Move_player(){
 	for (i = 0; i < counter; i++){		
 		var currentTile = player.parentNode;
 		var currentRow= currentTile.parentNode;
@@ -61,6 +166,7 @@ function Roll(){
 	if (counter !== 6) {
 		turn=turn+1
 	}	
-	
-	//console.log(turn);
 }
+	
+	
+	
